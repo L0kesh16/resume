91c0c565e1ac3a84781c4c213f98d9c9756d0776
Lokesh <lokiilokesh16@gmail.com>
2023-04-05 21:25:48 +0530

    Redeloy

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { HashRouter } from 'react-router-dom';
 import { BrowserRouter,Routes,Route} from 'react-router-dom'
 import Home from "./pages/Home";
 import Education from "./pages/Education";
