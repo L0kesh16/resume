0be04c5821d5ff51aa45e5bc99812e495f0123dd
Lokesh <lokiilokesh16@gmail.com>
2023-04-05 21:53:56 +0530

    homepage name changed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { HashRouter } from 'react-router-dom';
-import { BrowserRouter,Routes,Route} from 'react-router-dom'
+import { Routes,Route} from 'react-router-dom'
 import Home from "./pages/Home";
 import Education from "./pages/Education";
 import Experience from "./pages/Experience";
